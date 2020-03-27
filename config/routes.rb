4db931c579290d224f1ae5758e5dda427d5054f5
akairen <jimequeen@outlook.jp>
2020-03-27 17:44:09 +0900

    HTML入れ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index' => "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "posts/index" => "posts#index"
+
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+
+  get "/" => "home#top"
 end
