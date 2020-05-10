4321e858709b88458f801957ed0b73728508b062
akairen <jimequeen@outlook.jp>
2020-05-11 07:59:09 +0900

    routes うまくいくか検証

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,9 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+def login
 
+end
   # GET /resource/sign_in
   # def new
   #   super
