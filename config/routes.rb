1e6fb842aeccbb5f8d6a747969b372a81c7e4937
akairen <jimequeen@outlook.jp>
2020-06-08 08:37:13 +0900

    ログアウト機能修正（完了）

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,5 @@ Rails.application.routes.draw do
     post 'login', to: 'devise/sessions#create'
     delete 'signout', to: 'devise/sessions#destroy'
     post "signup" => "devise/sessions#signup"
-    get 'logout' => 'devise/sessions#destroy'
+    get '/users/sign_out' => 'devise/sessions#destroy'
 end
